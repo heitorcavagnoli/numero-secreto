e20848b640304496d01f9e30960e07c3fc315b92
Heitor Cavagnoli <heitor_cavagnoli@hotmail.com>
2024-04-16 09:10:10 -0300

    Alterando limite para 50

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+
+//Usando VSCode
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -11,12 +13,10 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
-
-//teste
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
