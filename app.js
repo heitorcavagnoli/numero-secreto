e9742ba9ad2bd81c2af0765573929107b59472a9
Heitor Cavagnoli <heitor_cavagnoli@hotmail.com>
2024-04-17 20:07:51 -0300

    Revert "Alterando limite para 50"
    This reverts commit e20848b640304496d01f9e30960e07c3fc315b92.

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
-
-//Usando VSCode
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -13,10 +11,12 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
+
+//teste
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
